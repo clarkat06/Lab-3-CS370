f4b698f20bbba87064f5bbec4918f583da52e91e
Andrew Clark <clarkat@hendrix.edu>
2025-02-26 00:22:39 -0600

    Implemented title screen

---
+++ Lab 3/Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    public void ReactToClick() {
+        Debug.Log("I've been clicked");
+        Initiate.Fade("Platformer", Color.black, 1.0f);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
